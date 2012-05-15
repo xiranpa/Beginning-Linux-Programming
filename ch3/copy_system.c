4bdd0aeaf3880b2bf4c216044b9e8ce49616d04c
Arthur <xiranpa@gmail.com>
2012-05-15 11:44:26 +0800

    added space

--- ch3/copy_system.c
+++ ch3/copy_system.c
@@ -10,7 +10,7 @@ int main()
 
 	in = open("file.in", O_RDONLY);
 	out = open("file.out", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-	while(read(in, &c, 1) == 1)
+	while (read(in, &c, 1) == 1)
 		write(out, &c, 1);
 
 	exit(0);
